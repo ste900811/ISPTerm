ae93eea1fd44916a13ad94c5d620cccd03537ab4
Yiyang Lu <ste900811@gmail.com>
2023-11-15 00:55:25 -0500

    Add NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Sidebar from "./Components/Sidebar.js";
 
 function App() {
   return (
     <div className="App">
-
+      <Sidebar />
     </div>
   );
 }
---
+++ frontend/src/Components/SidebarData.js
@@ -0,0 +1,21 @@
+import HomeIcon from "../Pictures/HomeIcon.png"
+import AddDeleteIcon from "../Pictures/AddDeleteIcon.png"
+import Kcal from "../Pictures/Kcal.png"
+
+export const SidebarData = [
+  {
+    title: "Home",
+    icon: HomeIcon,
+    link: "/home"
+  },
+  {
+    title: "Add/Delete",
+    icon: AddDeleteIcon,
+    link: "/addDelete"
+  },
+  {
+    title: "Cal Calculator",
+    icon: Kcal,
+    link: "/calCalculator"
+  }
+]
