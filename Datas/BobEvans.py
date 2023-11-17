76853bcc12501feb1a756466ce6048ed14dcfaab
Yiyang Lu <ste900811@gmail.com>
2023-11-17 17:07:40 -0500

    Add BobEvans and McDonalds data

---
+++ Datas/BobEvans.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+storeName = []
+mealName = []
+calories = []
+price = []
+lineCount = 1
+
+f = open("DataBobEvans.txt", "r")
+Lines = f.readlines()
+for line in Lines:
+
+    # parse the mean name and add into meanName, and generate storeName
+    if lineCount % 2 == 1:
+        storeName.append("BobEvan's")
+        mealName.append(line)
+    
+    # parsed calories and price and add into calories and price
+    elif lineCount % 2 == 0:
+        line = line.split(" ")
+        calories.append(line[2])
+        price.append(line[1])
+    lineCount += 1
+
+f.close()
+
+print("mealName Length: ", len(mealName))
+print("calories Length: ", len(calories))
+print("price Length: ", len(price))
+
+df = pd.DataFrame([storeName, mealName, calories, price]).T
+df.to_excel(excel_writer = "ExcelBobEvans.xlsx")
---
+++ Datas/parseMcDonalds.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+storeName = []
+mealName = []
+calories = []
+price = []
+lineCount = 1
+
+f = open("DataMcDonalds.txt", "r")
+Lines = f.readlines()
+for line in Lines:
+  line = line.replace("\n", "")
+  line = line.replace("\t", " ")
+  line = line.split(" ")
+
+  if line[-1][0] == "$" and line[-2] != "0" and line[-2] != "":
+    print(line)
+    storeName.append("McDonald's")
+    mealName.append(" ".join(line[:-2]))
+    calories.append(line[-2])
+    price.append(line[-1][1:])
+
+f.close()
+
+print("mealName Length: ", len(mealName))
+print("calories Length: ", len(calories))
+print("price Length: ", len(price))
+
+df = pd.DataFrame([storeName, mealName, calories, price]).T
+df.to_excel(excel_writer = "ExcelMcDonalds.xlsx")
