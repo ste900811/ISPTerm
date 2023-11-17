d2828055c1cbe5b5490a75e4dbf21fdda25b4049
Yiyang Lu <ste900811@gmail.com>
2023-11-16 20:01:08 -0500

    Add css into CalCalculator.jsx and App.css

--- frontend/src/Pages/CalCalculator.jsx
+++ frontend/src/Pages/CalCalculator.jsx
@@ -3,19 +3,22 @@ import { useState } from 'react';
 import '../App.css';
 
 export const CalCalculator = () => {
+  const [showText, setshowText] = useState(false);
   const [calories, setCalories] = useState(0);
   const [breakfastCalories, setBreakfastCalories] = useState(0); 
-  const [LunchDinnerCalories, setLunchDinnerCalories] = useState(0); 
+  const [LunchDinnerCalories, setLunchDinnerCalories] = useState(0);
+
   function getCalories() {
     let age = document.getElementById("age").value;
     let gender = document.getElementById("gender").value;
-    if (age == "None") {alert("Please select age."); return;}
-    if (gender == "None") {alert("Please select gender."); return;}
+    if (age === "None") {alert("Please select age."); return;}
+    if (gender === "None") {alert("Please select gender."); return;}
     let url = `http://localhost:3002/cal/${gender}/${age}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        setshowText(true);
         setCalories(data[0].calories);
         setBreakfastCalories(data[0].calories*0.2);
         setLunchDinnerCalories(data[0].calories*0.4);
@@ -25,7 +28,7 @@ export const CalCalculator = () => {
   return(
     <div>
       <div className="App">
-        <h1>Calories Calculator</h1>
+        <div id="title">Calories Calculator</div>
 
         <select className="option" id="age">
           <option value="None">Select Age</option>
@@ -39,20 +42,24 @@ export const CalCalculator = () => {
         </select>
         &emsp;&emsp;
         <select className="option" id="gender">
-          <option value="None">Select Age</option>
+          <option value="None">Select Gender</option>
           <option value="male">Male</option>
           <option value="female">Female</option>
         </select>
 
-        <br></br><br></br><br></br>
+        <br></br><br></br>
 
         <button onClick={getCalories}>Calculate</button>
 
-        <br></br><br></br>
+        <br></br>
 
-        <h2>Suggestion daily Calories: {calories}</h2>
-        <h2>Suggestion breakfast Calories: {breakfastCalories}</h2>
-        <h2>Suggestion lunch/dinner Calories: {LunchDinnerCalories}</h2>
+        {showText &&
+          <div>
+            <h2>Suggestion daily Calories: <span style={{color:"red"}}>{calories}</span></h2>
+            <h2>Suggestion breakfast Calories: {breakfastCalories}</h2>
+            <h2>Suggestion lunch/dinner Calories: {LunchDinnerCalories}</h2>
+          </div>
+        }
       </div>
     </div>
   );
